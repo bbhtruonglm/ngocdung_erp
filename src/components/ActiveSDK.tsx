da2a012c92cb878bbbb7ef571a48637ee2300693
bbhtruonglm <truonglm.bbh@gmail.com>
2026-01-27 16:28:47 +0700

    update

--- src/components/ActiveSDK.tsx
+++ src/components/ActiveSDK.tsx
@@ -96,8 +96,9 @@ const ActiveSDK = () => {
     }
 
     try {
+      // 'https://chatbox-app.botbanhang.vn/v1/app/app-installed/update'
       const END_POINT =
-        'https://chatbox-app.botbanhang.vn/v1/app/app-installed/update'
+        'https://cwo4kg8s8okss448sgo4o4c8.35.198.216.143.sslip.io/v1/app/app-installed/update'
 
       const response = await fetch(END_POINT, {
         method: 'POST',
